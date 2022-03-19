4d6cde4d13f733c8edc93287c18844dd4620e9fa
Gabriella Barbosa <gabsbarbosa852@gmail.com>
2022-03-19 13:44:42 -0300

    atualizações

--- hackathon-iteris/src/views/HomeView.vue
+++ hackathon-iteris/src/views/HomeView.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="HomeView">
     <v-container>
+    <br>
       <h2 class="text-h5 text-center mb-3 mt-5">Ovos de Páscoa</h2>
+      <br>
       <p>O ovo de pascoa caseiro feito com muito amor e carinho.</p>
+      <br>
     </v-container>
-    <div>
-    <v-btn color="secondary" elevation="2" large x-large>Cadastre-se</v-btn>
-    </div>
+     <v-img
+      class="inicio-imagem mb-4"
+      src="https://claudia.abril.com.br/wp-content/uploads/2020/01/coelho-1.jpg">
+    </v-img>
     <br>
     <div>
-    <v-btn color="secondary" elevation="2" large x-large>Login</v-btn>
+      <v-btn color="dark" elevation="2" large x-large>  CADASTRA-SE  </v-btn>
+    </div>
+    <br>
+    <br>
+    <div>
+      <v-btn color="dark" elevation="2" large x-large>     LOGIN     </v-btn>
     </div>
   </div>
 </template>
