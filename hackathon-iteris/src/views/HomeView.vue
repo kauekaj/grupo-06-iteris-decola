30e74534f7bce67a0955b2d204174059a49e286c
Gabriella Barbosa <gabsbarbosa852@gmail.com>
2022-03-19 13:07:08 -0300

    atualizacao

--- hackathon-iteris/src/views/HomeView.vue
+++ hackathon-iteris/src/views/HomeView.vue
@@ -1,1 +1,20 @@
+<template>
+  <div class="about">
+    <v-container>
+      <h2 class="text-h5 text-center mb-3 mt-5">Ovos de Páscoa</h2>
+      <p>
+        O ovo de pascoa caseiro feito com muito amor e carinho.
+      </p>
+    </v-container>
+  </div>
+</template>
 
+<script>
+export default {
+  name: 'HomeView'
+
+};
+</script>
+
+<style scoped>
+</style>
