64e996f6143677eac2be588e62136704471f72d6
Gabriella Barbosa <gabsbarbosa852@gmail.com>
2022-03-19 14:22:08 -0300

    atualizações

--- hackathon-iteris/src/views/HomeView.vue
+++ hackathon-iteris/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="HomeView">
     <v-container>
     <br>
-      <h2 class="text-h5 text-center mb-3 mt-5">Easter Egg Lover 🐰</h2>
+      <h2 class="text-h5 text-center mb-3 mt-5">EASTER EGG LOVER 🐰</h2>
       <br>
         <p>Difícil encontrar alguém que não se renda ao sabor incomparável e insubstituível do chocolate.</p> 
         <br>
@@ -11,18 +11,19 @@
         <p>Encontre aqui uma opção que caiba dentro do seu bolso e que seja irresistível, todos vão gostar.</p>
       <br>
     </v-container>
-     <v-img
-      class="inicio-imagem mb-4"
-      src="https://claudia.abril.com.br/wp-content/uploads/2020/01/coelho-1.jpg">
-    </v-img>
-    <br>
-    <div>
+        <div>
       <v-btn color="dark" elevation="2" large x-large>  CADASTRA-SE  </v-btn>
     </div>
     <br>
     <div>
       <v-btn color="dark" elevation="2" large x-large>     LOGIN     </v-btn>
     </div>
+    <br>
+     <v-img
+      class="inicio-imagem mb-4"
+      src="https://claudia.abril.com.br/wp-content/uploads/2020/01/coelho-1.jpg">
+    </v-img>
+    <br>
   </div>
 </template>
 
@@ -33,4 +34,13 @@ export default {
 </script>
 
 <style scoped>
+.inicio-imagem {
+  border-radius: 300px;
+}
+.HomeView {
+    background-color: khaki;
+}
+.botaoCadastro {
+    background-color: khaki;
+}
 </style>
