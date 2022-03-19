501dfa09412e3f2d5cb2f288c5fc9f6be0c7ed0c
Maria Eugênia Da Cruz <mariaebc21@gmail.com>
2022-03-19 14:54:18 -0300

    campos de login

--- hackathon-iteris/src/router/index.js
+++ hackathon-iteris/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import LoginView from '../views/LoginView.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,11 +11,14 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/LoginView',
+    name: 'LoginView',
+    component: LoginView
+  },
+  {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
